3b97ae52bd4756b7b81320c95a38749e1ff3e84c
Jason Livesay <runvnc@gmail.com>
2024-12-30 00:40:06 -0600

    update

--- src/mr_gemini/mod.py
+++ src/mr_gemini/mod.py
@@ -23,6 +23,7 @@ async def stream_chat(model, messages=[], context=None, num_ctx=200000,
         stream = await client.chat.completions.create(
             model=model_name,
             messages=messages,
+            response_format: { "type": "json_object" },
             stream=True,
             temperature=temperature,
             max_tokens=max_tokens
